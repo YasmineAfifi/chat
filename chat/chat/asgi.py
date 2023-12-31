4cb3dabc542294f2c76e89a80c3dac5f67775217
Yasmine <ynremotecoders@gmail.com>
2023-12-31 09:18:44 +0200

    final

--- chat/chat/asgi.py
+++ chat/chat/asgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from chatApp.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 
@@ -18,4 +20,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
 })
--- chat/chatApp/views.py
+++ chat/chatApp/views.py
@@ -17,17 +17,11 @@ def Room(request,room_name):
         
         try:
             search_room = ChatRoom.objects.get(name=room_name)
-            print('search_room')
-            print(search_room)
-            print('search_room')
             chats =[]
         except ChatRoom.DoesNotExist:
                 search_room = ChatRoom(name=room_name)
                 search_room.save()
         chats = Chat.objects.filter(room = search_room)
-        print('chats')
-        print(chats)
-        print('chats')
         return render(request,'chatApp/chatRooms.html',{'room':room_name,'chats':chats})
 
 
